013aaeda537545543ae6e856ea84bcae419b84b1
Jonas <jonas.nyvold02@gmail.com>
2025-06-10 23:17:42 +0200

    Initial commit

---
+++ TabFast/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TabFast
+//
+//  Created by Jonas Berger Nyvold on 10/06/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ TabFastTests/TabFastTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabFastTests.swift
+//  TabFastTests
+//
+//  Created by Jonas Berger Nyvold on 10/06/2025.
+//
+
+import Testing
+@testable import TabFast
+
+struct TabFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
